13cb04c17e961e4cb3024fada50bd2397f39ec0a
Viduruwan-55 <ypviduruwan55@gmail.com>
2025-03-15 09:55:34 +0530

    add logo images

--- portfolio/src/assets/index.js
+++ portfolio/src/assets/index.js
@@ -4,8 +4,8 @@ import projectOne from "./images/projects/project_1.png";
 import projectTwo from "./images/projects/project_2.JPG";
 import projectThree from "./images/projects/project_3.png";
 import projectFour from "./images/projects/project_4.png";
-import testimonialOne from "./images/logo_2.png";
-import testimonialTwo from "./images/logo_2.png";
+import testimonialOne from "./images/testmonial/IEEE_cs.jpg";
+import testimonialTwo from "./images/testmonial/ideaForge_logo.jpg";
 import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg_2.jpg";
 
