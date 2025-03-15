0a105878e4ece2b137319d53ae3682e6e60186c9
Viduruwan-55 <ypviduruwan55@gmail.com>
2025-03-15 18:59:32 +0530

    add project img

--- portfolio/src/assets/index.js
+++ portfolio/src/assets/index.js
@@ -4,6 +4,7 @@ import projectOne from "./images/projects/project_1.png";
 import projectTwo from "./images/projects/project_2.JPG";
 import projectThree from "./images/projects/project_3.png";
 import projectFour from "./images/projects/project_4.png";
+import projectFive from "./images/projects/project_5.png";
 import testimonialOne from "./images/testmonial/IEEE_cs.jpg";
 import testimonialTwo from "./images/testmonial/ideaForge_logo.jpg";
 import quote from "./images/testmonial/quote.png";
@@ -16,6 +17,7 @@ export {
   projectTwo,
   projectThree,
   projectFour,
+  projectFive,
   testimonialOne,
   testimonialTwo,
   quote,
