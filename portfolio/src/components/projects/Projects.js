b51b771ad8e6818b30bdda6bd6b637754edb651f
Viduruwan-55 <ypviduruwan55@gmail.com>
2025-03-16 16:42:14 +0530

    add links

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -33,12 +33,17 @@ const Projects = () => {
         />
         <ProjectsCard
           title="Student Attendance System"
-          des=""
+          des="An automated solution for real-time attendance tracking, improving accuracy and efficiency in the Faculty of Computing and Technology. (Group Project)"
           src={projectThree}
           git_link="https://github.com/Viduruwan-55/Student_Attendance_System.git"
+          link="https://github.com/PinsaraPerera/Student_Attendace_System.git"
         />
         {/* <ProjectsCard title="E-commerce Website" src={projectFour} /> */}
-        <ProjectsCard title="mental_health_chatbot" des=" " src={projectFive} />
+        <ProjectsCard
+          title="mental_health_chatbot"
+          des="Our group project, Smart Mental Health Chatbot, uses AI, NLP, and emotion recognition to provide personalized, empathetic support, offering real-time mental health assistance via a user-friendly mobile app."
+          src={projectFive}
+        />
       </div>
     </section>
   );
